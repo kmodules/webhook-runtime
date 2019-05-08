962d01212c5bf44ba9091b93d30f25e0bf5b555b
Tamal Saha <tamal@appscode.com>
2019-05-08 02:49:45 -0700

    Update hack/codegen.sh

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -13,7 +13,7 @@ pushd $REPO_ROOT
 docker run --rm -ti -u $(id -u):$(id -g) \
     -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
     -w "$DOCKER_REPO_ROOT" \
-    appscode/gengo:release-1.13 deepcopy-gen \
+    appscode/gengo:release-1.14 deepcopy-gen \
     --go-header-file "hack/gengo/boilerplate.go.txt" \
     --input-dirs "$PACKAGE_NAME/apis/workload/v1" \
     --output-file-base zz_generated.deepcopy
